0113149aa099a477aa2153ce316f1a1f7db82b35
Andrew Su <andrew.tiger.su@gmail.com>
2022-02-19 08:13:06 -0800

    Initial Commit

---
+++ NutriFirst/NutriFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriFirstApp.swift
+//  NutriFirst
+//
+//  Created by suandrew on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct NutriFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
